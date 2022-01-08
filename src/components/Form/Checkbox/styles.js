bde783b492616f81b9413d5884d30dc0245331b1
Larissa Tardivo <larissatardivo81@gmail.com>
2022-01-08 17:10:08 -0300

    indented code

--- src/components/Form/Checkbox/styles.js
+++ src/components/Form/Checkbox/styles.js
@@ -3,14 +3,14 @@ import { createStyles, makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) =>
   createStyles({
     checkbox: {
-        '& .MuiCheckbox-colorSecondary.Mui-checked': {
-            color: '#9683CD'
-        },
-        "& .MuiCheckbox-root": {
-            padding: '0',
-            verticalAlign: 'initial'
-        },
-    }
+      "& .MuiCheckbox-colorSecondary.Mui-checked": {
+        color: "#9683CD",
+      },
+      "& .MuiCheckbox-root": {
+        padding: "0",
+        verticalAlign: "initial",
+      },
+    },
   })
 );
 
