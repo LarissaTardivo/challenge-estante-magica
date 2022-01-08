8992ea6c25fce73e258ca2dc7cdb5529e2631466
Larissa Tardivo <larissatardivo81@gmail.com>
2022-01-08 17:08:00 -0300

    created register component

---
+++ src/pages/Register/index.js
@@ -0,0 +1,75 @@
+import React, { memo } from "react";
+
+import Grid from "@material-ui/core/Grid";
+
+import TeacherIcon from "../../assets/Images/icone_prof2.png";
+import ManagerIcon from "../../assets/Images/icone_gestor3.png";
+import ParentsIcon from "../../assets/Images/icone_familia2.png";
+
+import useStyles from "./styles";
+import { NavLink } from "react-router-dom";
+
+import { useFormik } from "formik";
+import Form from "./Form";
+import { useLocation } from "react-router-dom";
+import RegisterButtons from "./Buttons/Register";
+
+const Register: React.FC = () => {
+  const formik = useFormik({
+    initialValues: {
+      location: "",
+    },
+    validateOnMount: true,
+    onSubmit: async (values) => {
+      try {
+        console.log(values);
+      } catch (error) {}
+    },
+  });
+
+  const classes = useStyles();
+  const location = useLocation();
+  const value = location.pathname;
+  return (
+    <div className={classes.register}>
+      <Grid item xs={12} className="content">
+        <div className="card">
+          <div>
+            {value === "/teacher" ? <img src={TeacherIcon} alt="" /> : null}
+            {value === "/manager" ? <img src={ManagerIcon} alt="" /> : null}
+            {value === "/parents" ? <img src={ParentsIcon} alt="" /> : null}
+          </div>
+          <div>
+            {value === "/teacher" ? <h3>Criar conta de professor(a)</h3> : null}
+            {value === "/manager" ? <h3>Criar conta de gestor(a)</h3> : null}
+            {value === "/parents" ? <h3>Olá, Responsável!</h3> : null}
+          </div>
+          {value === "/parents" ? (
+            <div className="parents-info">
+              <span style={{ fontWeight: "400" }}>
+                Só é possível aplicar o projeto por uma escola. Mas você pode
+                acompanhar tudo de perto, recebendo nossos conteúdos!
+              </span>
+            </div>
+          ) : (
+            <div className="sub-info">
+              <span>
+                Se você já tiver uma conta,
+                <NavLink to="#" style={{ color: "#6C57A8" }}>
+                  {" "}
+                  faça seu login.
+                </NavLink>
+              </span>
+            </div>
+          )}
+          <form onSubmit={formik.handleSubmit}>
+            <Form formik={formik} value={value} />
+          </form>
+          <RegisterButtons />
+        </div>
+      </Grid>
+    </div>
+  );
+};
+
+export default memo(Register);
