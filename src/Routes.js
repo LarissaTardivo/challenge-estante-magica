72fd6a5f993f3d50ff31bc0fd5bf8cb1db926491
Larissa Tardivo <larissatardivo81@gmail.com>
2022-01-09 14:31:50 -0300

    added route to welcome and error

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from "./pages/Main";
 import Register from "./pages/Register";
+import Welcome from "./pages/Welcome";
+import Error from "./pages/Error";
 
 export default () => {
     return(
@@ -10,6 +12,8 @@ export default () => {
             <Route path='/teacher' element={<Register/>}/>
             <Route path='/manager' element={<Register/>}/>
             <Route path='/parents' element={<Register/>}/>
+            <Route path='/welcome' element={<Welcome/>}/>
+            <Route path='/error' element={<Error/>}/>
         </Routes>
     );
 }
