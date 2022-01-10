ec90ca08aef82b900900fda51b13dad16b757183
Larissa Tardivo <larissatardivo81@gmail.com>
2022-01-10 13:09:23 -0300

    added popup styles

--- src/pages/Register/Form/styles.js
+++ src/pages/Register/Form/styles.js
@@ -104,6 +104,44 @@ const useStyles = makeStyles((theme) =>
           display: "flex",
         },
       },
+      "& .box": {
+        position: "relative",
+        zIndex: "1"
+      },
+      "& .icon": {
+        padding: "2px 5px",
+        width: "fit-content",
+        marginLeft: "50px",
+        marginBottom: "-26px",
+        cursor: "pointer"
+      },
+      "& .box:hover .popup": {
+        opacity: 1,
+        display: "block"
+      },
+      "& .popup": {
+        padding: "1rem",
+        fontSize: '14px',
+        color: '#747474',
+        textAlign: 'center',
+        width: '341px',
+        height: '82px',
+        backgroundColor: "#FFF",
+        position: "absolute",
+        top: "0",
+        left: "5rem",
+        transaction: "all 0.25s ease",
+        opacity: "0",
+        display: "none",
+        border: '2px solid #E6DCFF',
+        borderRadius: '10px',
+        marginTop: '-3rem',
+        [theme.breakpoints.down("xs")]: {
+          width: '180px',
+          height: "150px",
+          marginTop: '-5rem',
+        },
+      }
     },
   })
 );
