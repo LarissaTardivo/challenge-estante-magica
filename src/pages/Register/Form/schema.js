bacd3b94145adcd80d79b8f9837de95b3206a23d
Larissa Tardivo <larissatardivo81@gmail.com>
2022-01-10 19:25:19 -0300

    fixed schema

--- src/pages/Register/Form/schema.js
+++ src/pages/Register/Form/schema.js
@@ -2,7 +2,6 @@ import * as yup from "yup";
 
 export default yup.object().shape({
   name: yup.string().required("Campo obrigatório"),
-  school_type: yup.string().required("Campo brigatório"),
-  email: yup.string().required("E-mail inválido"),
-  password: yup.string().required("Campo obrigatório")
+  email: yup.string().required("Campo obrigatório").email("Insira um e-mail válido"),
+  password: yup.string().required("Campo obrigatório").min(6, "Sua senha deve conter pelo menos 6 dígitos.")
 });
